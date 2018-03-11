45e9d696e5fe3713a26a292623a7f58d13f6e7be
Ravi <Ravi@01HW450845>
2018-03-11 14:31:40 +0530

    first

---
+++ src/com/my/git/GitMain.java
@@ -0,0 +1,12 @@
+package com.my.git;
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my first ever project in GIT");
+
+	}
+
+}
